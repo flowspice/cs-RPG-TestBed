991dc329f2cbabcae5f2b23ea03db54148c3b7ad
Luke Hudgins <luke.p.w.hudgins@gmail.com>
2020-11-14 18:59:31 -0400

    Add project files.

---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,8 @@
+﻿namespace Engine
+{
+    public class LivingCreature
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+    }
+}
---
+++ Engine/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace Engine
+{
+    public class Player : LivingCreature
+    {
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+    }
+}
